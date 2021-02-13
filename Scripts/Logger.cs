19e32c33cbcc3fa13e3c258bf6ee79e19480518e
Kyle Conibear <kyle.a.conibear@gmail.com>
2021-02-12 23:52:55 -0500

    Removing enum log type

--- Scripts/Logger.cs
+++ Scripts/Logger.cs
@@ -1,63 +1,49 @@
-﻿using System.Diagnostics;
-using System.Text;
+﻿using UnityEngine;
 
-namespace KyleConibear
-{
-    public static class Logger
-    {
-        // To be used in a comparison with the new log being created. If they are the same don't print the same log twice
-        private static string preivousLog = string.Empty;
+namespace Conibear {
+	using System.Diagnostics;
+	using System.Text;
 
-        public enum Type
-        {
-            Message = 0,
-            Warning = 1,
-            Error = 2
-        }
+	public static class Logger {
+		// To be used in a comparison with the new log being created. If they are the same don't print the same log twice
+		private static string preivousLog = string.Empty;
 
-        public static void Log(Type messageType = Type.Message, string message = "Method has executed.", bool isSinglePrint = false)
-        {
-            StackFrame frame = new StackFrame(1);
+		public static void Log(LogType logType = LogType.Log, string message = "Method has executed.", bool isSinglePrint = false) {
+			StackFrame frame = new StackFrame(1);
 
-            StringBuilder log = new StringBuilder();
+			StringBuilder log = new StringBuilder();
 
-            string callers_method_name = frame.GetMethod().Name;
+			string callers_method_name = frame.GetMethod().Name;
 
-            string callers_class_name = frame.GetMethod().DeclaringType.Name;
+			string callers_class_name = frame.GetMethod().DeclaringType.Name;
 
-            // Create signature
-            string signature = $"{callers_class_name}:{callers_method_name}-";
+			// Create signature
+			string signature = $"{callers_class_name}:{callers_method_name}-";
 
-            log.Append(signature + message);
+			log.Append(signature + message);
 
-            if (isSinglePrint)
-            {
-                if (log.ToString() != preivousLog)
-                {
-                    preivousLog = log.ToString();
-                    PrintLogByType(messageType, log.ToString());
-                }
-            }
-            else
-            {
-                PrintLogByType(messageType, log.ToString());
-            }
-        }
+			if (isSinglePrint) {
+				if (log.ToString() != preivousLog) {
+					preivousLog = log.ToString();
+					PrintLogByType(logType, log.ToString());
+				}
+			} else {
+				PrintLogByType(logType, log.ToString());
+			}
+		}
 
-        private static void PrintLogByType(Type type, string message)
-        {
-            switch (type)
-            {
-                case Type.Message:
-                UnityEngine.Debug.Log(message);
-                break;
-                case Type.Warning:
-                UnityEngine.Debug.LogWarning(message);
-                break;
-                case Type.Error:
-                UnityEngine.Debug.LogError(message);
-                break;
-            }
-        }
-    }
+		private static void PrintLogByType(LogType logType, string message) {
+			switch (logType) {
+				case LogType.Log:
+					UnityEngine.Debug.Log(message);
+					break;
+				case LogType.Warning:
+					UnityEngine.Debug.LogWarning(message);
+					break;
+				case LogType.Error:
+					UnityEngine.Debug.LogError(message);
+					break;
+			}
+		}
+	}
 }
